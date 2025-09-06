ea84fef570286b482ccdaa1fe54fb6a1b914c71c
shikharyadav16 <yadavshikhar49@gmail.com>
2025-09-07 01:37:29 +0530

    update some files

--- Connection.js
+++ Connection.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 async function connectToDb() {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/nextscrimz");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Database connected Successfully!");
     } catch(err) {
         console.log("Error", err);
