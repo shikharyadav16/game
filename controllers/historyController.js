fc60ac25c7ad20d6277127143d2de258e2c26e27
shikharyadav16 <yadavshikhar49@gmail.com>
2025-09-15 11:52:16 +0530

    changed ranking options

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -7,10 +7,27 @@ async function handleGetHistory(req, res) {
   try {
     const user = await User.findById(_id);
     const wallet = user.wallet;
+    let matchesPlayed = user.registeredArray.length;
+    if (!matchesPlayed) {
+      matchesPlayed = 0;
+    }
+    const maxWin = user.maxWin;
+    const totalWin = user.totalWin;
+    const rank =
+      (await User.countDocuments({ totalWin: { $gt: totalWin } })) + 1;
 
     const results = await Result.find({ user: _id });
 
-    return res.status(200).render("history.ejs", { results, wallet });
+    return res
+      .status(200)
+      .render("history.ejs", {
+        results,
+        wallet,
+        maxWin,
+        rank,
+        totalWin,
+        matchesPlayed,
+      });
   } catch (err) {
     console.log("Error:", err);
     res.status(500).json({ success: false, message: err.message });
