0b68db861fa80ee35b10d9c976a43b0db7f44508
shikharyadav16 <yadavshikhar49@gmail.com>
2025-09-22 23:22:45 +0530

    some small changes

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use((req, res)=> {
 })
 
 
-const port = 3000;
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
